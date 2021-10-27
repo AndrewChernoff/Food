06c09c9ecc6f08e0bb2293ef3ec7841f1491c5ca
Andrew <whitedrew538@gmail.com>
2021-10-28 00:14:20 +0500

    classes

--- js/script2.js
+++ js/script2.js
@@ -170,3 +170,60 @@ function showModalByScroll() {
 }
 
 window.addEventListener('scroll', showModalByScroll);
+
+class MenuCard {
+    constructor(img, title, descr, price, parentSelector) {
+        this.img = img;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.convert = 27;
+        this.toUAH();
+    }
+
+    toUAH() {
+        this.price = this.price * this.convert
+    }
+    render() {
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="menu__item">
+        <img src=${this.img} alt="vegy">
+        <h3 class ="menu__item-subtitle">${this.title}</h3>
+        <div class ="menu__item-descr">${this.descr}</div>
+        <div class ="menu__item-divider"></div>
+        <div class ="menu__item-price">
+        <div class ="menu__item-cost">Цена: </div>
+        <div class ="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+    </div>`
+        this.parent.append(div);
+    }
+}
+
+new MenuCard(
+    "img/tabs/elite.jpg",
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    229,
+    '.menu .container'
+).render();
+
+new MenuCard(
+    "img/tabs/vegy.jpg",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    550,
+    '.menu .container'
+).render();
+
+new MenuCard(
+    "img/tabs/post.jpg",
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    430,
+    '.menu .container'
+).render();
+
+
+
