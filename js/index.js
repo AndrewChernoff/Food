7e7c2b483849f54bf8b8110249ee1983221e0f0f
Andrew <whitedrew538@gmail.com>
2021-12-18 00:09:10 +0500

    navigation

--- js/index.js
+++ js/index.js
@@ -363,6 +363,9 @@ nextBtn.addEventListener('click', () => {
 
     idx--;
     showImgNumber();
+
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[idx - 1].style.opacity = 1;
 });
 
 prevBtn.addEventListener('click', () => {
@@ -375,4 +378,68 @@ prevBtn.addEventListener('click', () => {
 
     idx++;
     showImgNumber(idx);
+
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[idx - 1].style.opacity = 1;
 });
+
+////navigation for the slider
+const slider = document.querySelector('.offer__slider');
+slider.style.position = 'relative';
+
+const indicators = document.createElement('ol'),
+    dots = [];
+indicators.classList.add('carousel-indicators');
+indicators.style.cssText = ` position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;`;
+
+slider.append(indicators);
+
+for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.style.cssText = `
+    box-sizing: content-box;
+    flex: 0 1 auto;
+    width: 30px;
+    height: 6px;
+    margin-right: 3px;
+    margin-left: 3px;
+    cursor: pointer;
+    background-color: #fff;
+    background-clip: padding-box;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    opacity: .5;
+    transition: opacity .6s ease;
+    `;
+    if (i == 0) {
+        dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+}
+
+dots.forEach(dot => {
+    dot.addEventListener('click', (e) => {
+        const slideTo = e.target.getAttribute('data-slide-to');
+
+        idx = slideTo;
+        offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+        sliderInner.style.transform = `translateX(-${offset}px)`;
+
+        showImgNumber();
+
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[idx - 1].style.opacity = 1;
+    })
+}) 
